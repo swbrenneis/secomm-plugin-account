e22806c5cb1b96d5d421bb18a3c7a2c83b0351c5
Steve Brenneis <steve.brenneis@secomm.org>
2017-09-03 12:53:28 -0400

    Stubbed all methods.

--- src/ios/SeAccountPlugin.h
+++ src/ios/SeAccountPlugin.h
@@ -2,6 +2,16 @@
 
 @interface SeAccountPlugin : CDVPlugin
 
+- (void) closeSession:(CDVInvokedUrlCommand*)command;
+
+- (void) closeVault:(CDVInvokedUrlCommand*)command;
+
+- (void) createVault:(CDVInvokedUrlCommand*)command;
+
 - (void) openSession:(CDVInvokedUrlCommand*)command;
+
+- (void) finish:(CDVInvokedUrlCommand*)command;
+
+- (void) request:(CDVInvokedUrlCommand*)command;
 
 @end
--- src/ios/SeAccountPlugin.mm
+++ src/ios/SeAccountPlugin.mm
@@ -2,7 +2,22 @@
 
 @implementation SeAccountPlugin : CDVPlugin
 
+- (void) closeSession:(CDVInvokedUrlCommand*)command {
+}
+
+- (void) closeVault:(CDVInvokedUrlCommand*)command {
+}
+
+- (void) createVault:(CDVInvokedUrlCommand*)command {
+}
+
 - (void) openSession:(CDVInvokedUrlCommand*)command {
+}
+
+- (void) finish:(CDVInvokedUrlCommand*)command {
+}
+
+- (void) request:(CDVInvokedUrlCommand*)command {
 }
 
 @end
